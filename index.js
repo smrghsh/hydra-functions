4005b14296623528e46675a5caa8da06623595dd
Samir Ghosh <hello@samir.tech>
2024-04-12 11:34:09 -0700

    access inputs description from eaxmples
    Co-Authored-By: TheoC26 <91738250+theoc26@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 // import devtools from 'choo-devtools'
-import choo from 'choo'
+import choo from "choo";
+// import devtools from "choo-devtools";
+import store from "./stores/store.js";
+import i18next from "i18next";
+import i18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 
-import store from './stores/store.js'
-import i18next from 'i18next'
-import i18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+import languageResources from "./locales.js";
 
-import languageResources from './locales.js'
-
-i18next
-.use(i18nextBrowserLanguageDetector)
-.init({
+i18next.use(i18nextBrowserLanguageDetector).init({
   debug: true,
-  fallbackLng: 'en',
+  fallbackLng: "en",
   resources: languageResources,
-})
+});
 
-var app = choo({ hash: true })
-// app.use(devtools())
-app.use(store)
+var app = choo({ hash: true });
+// app.use(devtools());
+app.use(store);
 
-import mainView from './views/main.js'
+import mainView from "./views/main.js";
 
-app.route('/', mainView)
-app.route('/docs', mainView)
-app.route('/hydra-functions', mainView)
-app.route('#functions/:function/:tab', mainView)
-app.route('/docs/functions/:function/:tab', mainView)
-app.route('/hydra-functions/functions/:function/:tab', mainView)
+app.route("/", mainView);
+app.route("/docs", mainView);
+app.route("/hydra-functions", mainView);
+app.route("#functions/:function/:tab", mainView);
+app.route("/docs/functions/:function/:tab", mainView);
+app.route("/hydra-functions/functions/:function/:tab", mainView);
 
-app.route('/api', mainView)
-app.route('/api/functions/:function/:tab', mainView)
+app.route("/api", mainView);
+app.route("/api/functions/:function/:tab", mainView);
 
-app.route('/functions', mainView)
-app.route('/functions/functions/:function/:tab', mainView)
+app.route("/functions", mainView);
+app.route("/functions/functions/:function/:tab", mainView);
 
-app.mount('body')
+app.mount("body");
--- libs/hydra-reference.js
+++ libs/hydra-reference.js
@@ -1,83 +1,101 @@
-import hydraFunctions from './hydra/glsl-functions.js'
-import hydraPlugins from './hydra/hydra-plugins.js'
-import hydraTypes from './hydra/types.js'
-import examples from '../examples.js'
+import hydraFunctions from "./hydra/glsl-functions.js";
+import hydraPlugins from "./hydra/hydra-plugins.js";
+import hydraTypes from "./hydra/types.js";
+import examples from "../examples.js";
+// devtools
 
 class Item {
-  constructor ({ obj, colorIndex, category }) {
-    this.name = obj.name
-    this.category = category
-    this.colorIndex = colorIndex
-    this.inputs = obj.inputs
-    this.default = obj.default
+  constructor({ obj, colorIndex, category }) {
+    this.name = obj.name;
+    this.category = category;
+    this.colorIndex = colorIndex;
+    this.inputs = obj.inputs;
+    this.default = obj.default;
+    console.log(this.name);
 
-    if (this.category.type === 'combine' || this.category.type === 'combineCoord') {
-      this.inputs = [ { type: 'vec4', name: 'texture' }, ...this.inputs]
+    if (
+      this.category.type === "combine" ||
+      this.category.type === "combineCoord"
+    ) {
+      this.inputs = [{ type: "vec4", name: "texture" }, ...this.inputs];
     }
 
-    this.initExamples()
+    this.initExamples();
+    this.initInputsDescription();
   }
 
-  initExamples () {
-    let ref = examples[this.name]
+  initExamples() {
+    let ref = examples[this.name];
     if (ref === undefined || ref.example === undefined) {
       // functions that are not documented
-      this.undocumented = true
-      this.examples = []
-    }
-    else {
+      this.undocumented = true;
+      this.examples = [];
+    } else {
       if (Array.isArray(ref.example) === false) {
-        this.examples = [ref.example]
+        this.examples = [ref.example];
+      } else {
+        this.examples = ref.example;
       }
-      else {
-        this.examples = ref.example
+    }
+  }
+  initInputsDescription() {
+    let ref = examples[this.name];
+    if (ref === undefined || ref.example === undefined) {
+      // functions that are not documented
+      this.undocumented = true;
+      this.inputsDescription = [];
+    } else {
+      if (Array.isArray(ref.example) === false) {
+        this.inputsDescription = [ref.inputsDescription];
+      } else {
+        this.inputsDescription = ref.inputsDescription;
       }
     }
   }
 }
 
 class Category {
-  constructor ({ type, colorIndex, allFuncs }) {
-    this.type = type
-    this.colorIndex = colorIndex
-    this.funcs = []
-    const objList = allFuncs.filter((obj) => obj.type === type)//.sort((a, b) => a.name > b.name)
+  constructor({ type, colorIndex, allFuncs }) {
+    this.type = type;
+    this.colorIndex = colorIndex;
+    this.funcs = [];
+    const objList = allFuncs.filter((obj) => obj.type === type); //.sort((a, b) => a.name > b.name)
     for (const obj of objList) {
-      const item = new Item({ obj, colorIndex, category: this })
-      this.funcs.push(item)
+      const item = new Item({ obj, colorIndex, category: this });
+      this.funcs.push(item);
     }
   }
 }
 
 class HydraReference {
-  constructor () {
-    this.categories = []
-    this.allFuncs = [...hydraFunctions(), ...hydraPlugins]
-    this.allItems = []
+  constructor() {
+    this.categories = [];
+    this.allFuncs = [...hydraFunctions(), ...hydraPlugins];
+    this.allItems = [];
 
     for (const index in hydraTypes) {
-      const type = hydraTypes[index].key
+      const type = hydraTypes[index].key;
       const category = new Category({
         type,
         colorIndex: index,
         allFuncs: this.allFuncs,
-      })
-      this.categories.push(category)
-      this.allItems.push(...category.funcs)
+      });
+      this.categories.push(category);
+      this.allItems.push(...category.funcs);
     }
   }
 
-  getGroups () {
-    return this.categories
+  getGroups() {
+    return this.categories;
   }
 
-  getItem (name) {
-    return this.allItems.find(e => e.name === name)
+  getItem(name) {
+    return this.allItems.find((e) => e.name === name);
   }
 
-  getExamples (name) {
-    return this.getItem(name).examples
+  getExamples(name) {
+    return this.getItem(name).examples;
   }
 }
 
-export default () => new HydraReference
+export default () => new HydraReference();
